513c9fe7c47f5db5170ed82ced22603e514f7345
Clara Nascimento <clara.nasc13@gmail.com>
2026-06-15 17:05:04 -0300

    feat: implement sessions controller with JWT authentication logic

--- src/controllers/sessions-controller.ts
+++ src/controllers/sessions-controller.ts
@@ -1,9 +1,30 @@
-import { Request, Response } from "express"
+import { Request, Response } from "express";
+import { AppError } from "@/utils/AppError";
+import jwt from "jsonwebtoken";
+import { authConfig } from "@/configs/auth";
 
 class SessionsController {
   async create(request: Request, response: Response) {
-    return response.json({message: process.env.USER_NAME })
+    const { username, password } = request.body;
+
+    const fakeUser = {
+      id: "1",
+      username: "Ana Clara",
+      password: "123456",
+    };
+
+    if (fakeUser.username !== username || fakeUser.password !== password) {
+      throw new AppError("Usuário ou senha inválidos", 401);
+    }
+
+    const {secret, expiresIn} = authConfig.jwt;
+    const token = jwt.sign({ id: fakeUser.id }, secret, {
+      expiresIn,
+      subject: String(fakeUser.id),
+    });
+
+    return response.json({ token });
   }
 }
 
-export { SessionsController }
+export { SessionsController };
